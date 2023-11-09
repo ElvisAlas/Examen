1f77e03948dd072ba44c02c843e97faa8bdf7822
ElvisAlas <110005376+ElvisAlas@users.noreply.github.com>
2023-11-08 18:28:08 -0600

    creacion clase alumno

---
+++ Examen/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+    abstract class Alumno
+    {
+        public string Nombre { get; set; }
+        public string NumeroCuenta { get; set; }
+        public string Email { get; set; }
+
+        public Alumno(string nombre, string numeroCuenta, string email)
+        {
+            Nombre = nombre;
+            NumeroCuenta = numeroCuenta;
+            Email = email;
+        }
+
+        public void MostrarAlumno()
+        {
+            Console.WriteLine("Datos del Alumno:");
+            Console.WriteLine("Nombre: " + Nombre);
+            Console.WriteLine("Número de Cuenta: " + NumeroCuenta);
+            Console.WriteLine("Email: " + Email);
+        }
+
+    }
+}
